c0e88c0612ae46328dcf8658bd5de5b70dcc13fc
Yuuta Kaburagi <kabu.taku@gmail.com>
2016-01-26 03:30:12 +0900

    ハローコミットテスト

---
+++ testApp/src/testApp/TestAppMain.java
@@ -0,0 +1,15 @@
+package testApp;
+
+public class TestAppMain {
+
+	private static String GPM_URL = "https://secure.goldpoint.co.jp/gpm/membertop.html";
+	
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("hello world");
+		
+		
+	}
+
+}
